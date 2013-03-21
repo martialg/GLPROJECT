e46fb354c359f19c388637deae844749e832d841
Quentin Burtschell <quentin.burtschell@gmail.com>
2013-03-21 14:33:57 +0100

    Name Refactor
    convention de nommage non respecté

--- src/io/Read.java
+++ src/io/Read.java
@@ -22,21 +22,21 @@ import java.util.logging.Logger;
 public class Read {
 
     //Lecture du fichier
-    public void read(File inputFile) {
-        String nameFile = inputFile.getPath();
+    public void read(File input_file) {
+        String nameFile = input_file.getPath();
         if ("txt".equals(nameFile.substring(nameFile.length() - 3, nameFile.length()))) {
             try {
-                FileInputStream fis = new FileInputStream(inputFile);
+                FileInputStream fis = new FileInputStream(input_file);
                 InputStreamReader isr = new InputStreamReader(fis);
-                BufferedReader inputText = new BufferedReader(isr);
+                BufferedReader input_text = new BufferedReader(isr);
                 String line;
-                while ((line = inputText.readLine()) != null) {
+                while ((line = input_text.readLine()) != null) {
                     System.out.println(line + " " + line.length());
                     if (!("".equals(line))) {
                         this.graphCreation(line);
                     }
                 }
-                inputText.close();
+                input_text.close();
                 isr.close();
                 fis.close();
             } catch (FileNotFoundException ex) {
@@ -54,63 +54,63 @@ public class Read {
         /**
          * PARTIE TYPE RELATION
          */
-        int typeRelation = 0;
-        int typeLeft;                                                           //Localisation du début de la relation
+        int type_relation = 0;
+        int type_left;                                                           //Localisation du début de la relation
         if (line.indexOf("<") == -1) {
-            typeRelation = 1;
-            typeLeft = line.indexOf("-");
+            type_relation = 1;
+            type_left = line.indexOf("-");
         } else {
-            typeRelation = 2;
-            typeLeft = line.indexOf("<");
+            type_relation = 2;
+            type_left = line.indexOf("<");
         }
-        int typeRight = line.lastIndexOf(">");
+        int type_right = line.lastIndexOf(">");
 
         /**
          * PARTIE NOM
          */
-        String firstName = line.substring(0, typeLeft);
-        String lastName = line.substring(typeRight + 1, line.length());
+        String first_name = line.substring(0, type_left);
+        String last_name = line.substring(type_right + 1, line.length());
 
         /**
          * PARTIE ATTRIBUTS
          */
-        boolean haveArg;                                                        //indique si la liaison est de type Avec ou Sans Arg
-        int firstArg = line.indexOf("[");
+        boolean have_arg;                                                        //indique si la liaison est de type Avec ou Sans Arg
+        int first_arg = line.indexOf("[");
         //si il y a un argument
-        if (!(firstArg == -1)) {
+        if (!(first_arg == -1)) {
 
-            haveArg = true;
-            int lastArg = line.indexOf("]");
-            String args = line.substring(firstArg + 1, lastArg);                    //contient tous les arguments entre []
+            have_arg = true;
+            int last_arg = line.indexOf("]");
+            String args = line.substring(first_arg + 1, last_arg);                    //contient tous les arguments entre []
 
-            int endingArg = args.indexOf(",");
-            if (!(endingArg == -1)) {
+            int ending_arg = args.indexOf(",");
+            if (!(ending_arg == -1)) {
                 String[] arg = args.split(",");
                 for (String s : arg) {
-                    this.decomposeArgument(s);
+                    this.splitUpArgument(s);
                 }
             } else {
-                this.decomposeArgument(args);
+                this.splitUpArgument(args);
             }
 
         } else {
-            haveArg = false;
+            have_arg = false;
         }
 
         /**
          * PARTIE NOM RELATION
          */
-        String nameRelation = "";
+        String name_relation = "";
         int start;
-        if (typeRelation == 1) {
-            start = typeLeft + 2;
+        if (type_relation == 1) {
+            start = type_left + 2;
         } else {
-            start = typeLeft + 3;
+            start = type_left + 3;
         }
-        if (haveArg) {
-            nameRelation = line.substring(start, firstArg);
+        if (have_arg) {
+            name_relation = line.substring(start, first_arg);
         } else {
-            nameRelation = line.substring(start, (typeRight - 2));
+            name_relation = line.substring(start, (type_right - 2));
         }
 
         //Creer le graphe avec les fonctions du graphe qui seront coder
@@ -118,30 +118,27 @@ public class Read {
          * Rappel des variables : - typeRelation : 1 si un seul sens / 2 si
          * double - firstName : Premier nom de l'association - lastName :
          * Deuxieme nom de l'assocation - nameRelation : Nom de l'association -
-         * type / val sont dans la fonction decomposeArgument
+         * argument_type / argument_values sont dans la fonction decomposeArgument
          */
-        System.out.println(" resultat = " + typeRelation + "," + nameRelation + "," + firstName + "," + lastName);
-        System.out.println("--------------------------------------------------");
     }
 
     //decoupe un argument proprement pour l'ajouter au type de relation du graph
-    public void decomposeArgument(String arg) {
+    public void splitUpArgument(String arg) {
         int separator = arg.indexOf("=");
-
-        ArrayList<String> val = new ArrayList<String>();
-        String type = arg.substring(0, separator);
-
-        String values = arg.substring((separator + 1), arg.length());
-
+        String[] tab_val = null;
+        String argument_type = arg.substring(0, separator);
+        String argument_values = arg.substring((separator + 1), arg.length());
+        int iterator = 0;
         // Controle si il y a plusieurs valeurs (ex : objet1/objet2)
-        if (values.indexOf("/") == -1) {
-            val.add(values);
+        if (argument_values.indexOf("/") == -1) {
+            tab_val[iterator] = argument_values;
         } else {
-            String[] value = values.split("/");
-            for (String s : value) {
-                val.add(s);
+            String[] value = argument_values.split("/");
+            for (String current_value : value) {              
+                tab_val[iterator] = current_value;
+                iterator++;
             }
-        }
+        }  
         //ajouter le type et la valeur de l'argument au graph
     }
 }
