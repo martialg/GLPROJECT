ba082c8cdffe40c5e7925dfe2fc7bb5b7ca8f1fc
Romain Sergeant <romain.sergeant@gmail.com>
2013-03-21 14:48:42 +0100

    Initialisation du modèle de base
    Constructeurs, accesseurs, ajouts

---
+++ src/model/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @driver Romain
+ * @observer Martial
+ */
+public class Node {
+   
+    private String name;
+
+    public Node(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
