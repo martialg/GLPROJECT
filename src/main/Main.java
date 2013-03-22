784ba606eb5557a6f64a9e8c4f1c05176efca802
PARIZOT <parizot.etienne@gmail.com>
2013-03-22 09:34:23 +0100

    Finalisation Reader
    Correctif du NullPointer
    Test de la structure du Line

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,16 @@
 package main;
 
+import io.Read;
+
 public class Main {
+    
+    public static void main(String[] args){
+        Read lecture = new Read();
+        lecture.readFile("C:\\Users\\Caedes\\Dropbox\\Quentin&Martial&Romain&Etienne\\Genie Logiciel\\graph.txt");
+        for(int i = 0 ; i < lecture.getLines().size() ; i++){
+            System.out.println(lecture.getLines().get(i));
+        }
+    }
+    
     
 }
