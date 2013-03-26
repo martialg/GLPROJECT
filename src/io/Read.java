8acedbf5925a25a7a901b95fec6a27b3ef53db24
Quentin Burtschell <quentin.burtschell@gmail.com>
2013-03-26 11:49:04 +0100

    Test du split & modification de la fonction qui ne passait pas les tests

--- src/io/Read.java
+++ src/io/Read.java
@@ -81,25 +81,23 @@ public class Read {
         int first_arg = line.indexOf("[");
         //si il y a un argument
         if (!(first_arg == -1)) {
-
             have_arg = true;
             int last_arg = line.indexOf("]");
             String args = line.substring(first_arg + 1, last_arg);                    //contient tous les arguments entre []
-
             int ending_arg = args.indexOf(",");
             if (!(ending_arg == -1)) {
                 String[] arg = args.split(",");
                 for (String s : arg) {
-                    property_map = Read.splitUpProperties(s);
+                    Property temp = splitUpProperties(s);
+                    property_map.put(temp.getName(),temp);
                 }
             } else {
-                property_map = Read.splitUpProperties(args);
+                Property temp = splitUpProperties(args);
+                property_map.put(temp.getName(),temp);
             }
-
         } else {
             have_arg = false;
         }
-
         /**
          * PARTIE NOM RELATION
          */
@@ -115,13 +113,11 @@ public class Read {
         } else {
             name_relation = line.substring(start, (type_right - 2));
         }
-
         return (new Line(first_name, last_name, name_relation, type_relation, property_map));
-        
     }
 
     //decoupe un argument proprement pour l'ajouter au type de relation du graph
-    public static HashMap<String,Property> splitUpProperties(String arg) {
+    public static Property splitUpProperties(String arg) {
         int separator = arg.indexOf("=");
         String[] tab_val = null;
         String argument_type = arg.substring(0, separator);
@@ -138,10 +134,7 @@ public class Read {
                 tab_val[iterator] = current_value;
                 iterator++;
             }
-        }  
-        HashMap<String,Property> property_map = new HashMap<String,Property>();
-        Property properties = new Property(argument_type, tab_val);
-        property_map.put(argument_type, properties);
-        return property_map;
+        }
+        return new Property(argument_type, tab_val);
     }
 }
